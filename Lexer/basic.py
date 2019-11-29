c189a759a83e6b26c8aab7862bbbaa2d2562e1b8
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2019-11-29 08:26:43 +0800

    adding getting_started folder and modified Lexer folder

--- Lexer/basic.py
+++ Lexer/basic.py
@@ -91,23 +91,26 @@ class Lexer:
         tokens = []
 
         while self.current_char != None:
-            if self.current_char in ' \t':
+            if self.current_char in ' \t':          # TAB
                 self.advance()
             elif self.current_char in DIGITS:
                 tokens.append(self.make_number())
-            elif self.current_char == '+':
+            elif self.current_char == '+':          # PLUS
                 tokens.append((Token(TT_PLUS)))
                 self.advance()
-            elif self.current_char == '-':
+            elif self.current_char == '-':          # MINUS
                 tokens.append((Token(TT_MINUS)))
                 self.advance()
-            elif self.current_char == '*':
+            elif self.current_char == '/':          # DIVISION
+                tokens.append((Token(TT_DIV)))
+                self.advance()
+            elif self.current_char == '*':          # MULTIPLICATION
                 tokens.append((Token(TT_MUL)))
                 self.advance()
-            elif self.current_char == '(':
+            elif self.current_char == '(':          # LEFT PARENTHESIS
                 tokens.append((Token(TT_LPAREN)))
                 self.advance()
-            elif self.current_char == ')':
+            elif self.current_char == ')':          # RIGHT PARENTHESIS
                 tokens.append((Token(TT_RPAREN)))
                 self.advance()
             else:
@@ -138,11 +141,85 @@ class Lexer:
             return Token(TT_FLOAT, float(num_str))
 
 ##########
+# NODES
+##########
+
+class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+
+    def __repr__(self):
+        return f'{self.tok}'
+
+class BinOpNode:
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+    
+    def __repr__(self):
+        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+
+##########
+# PARSER
+##########
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_idx = 1
+        self.advance()
+
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+
+######################
+
+    def parse(self):
+        res = self.expr()
+        return res
+    
+    def factor(self):
+        tok = self.current_tok
+
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    def expr(self):
+        return self.bin_op(self.factor, (TT_PLUS, TT_MINUS))
+
+    ###############################
+
+    def bin_op(self, func, ops):
+        left = func()
+
+        while self.current_tok.type in ops:
+            op_tok = self.current_tok
+            self.advance()
+            right = func()
+            left = BinOpNode(left, op_tok, right)
+
+        return left
+
+##########
 # RUN
 ##########
 
 def run(fn, text):
+    # Generate tokens
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
+    if error: return None, error
 
-    return tokens, error
+    # Generate AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    
+    return ast, None
